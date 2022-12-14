efbbe345cce9b02eb7d9a1c14f9611c4647df4fd
SheyNjila <96470430+SheyNjila1@users.noreply.github.com>
2022-12-13 19:03:54 -0500

    Update Scripts

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,3 +1,4 @@
+```
 #!/bin/bash
 # Hardware requirements: AWS Linux 2 with mimum t2.micro type instance & port 8080(application port), 9100 (node-exporter port) should be allowed on the security groups
 # setup for the ansible configuration
@@ -33,3 +34,4 @@ sudo systemctl daemon-reload
 sudo systemctl enable node-exporter
 sudo systemctl start node-exporter
 sudo systemctl status node-exporter
+```
